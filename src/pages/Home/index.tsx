620b7fe3767d0fbe284bc4ca460efa8c4c29de0d
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2023-07-01 00:12:14 -0300

    Adicionando step e limitadores ao timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,17 +7,24 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+                    <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+                    <datalist id="task-suggestions">
+                        <option value="Estudar Inglês" />
+                        <option value="Estudar React" />
+                        <option value="Estudar Node" />
+                        <option value="Estudar React Native" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
-                     type="number"
-                     id="minutesAmount"
-                     placeholder="00"
-                     step={5}
-                     min={5}
-                     max={60}
-                     />
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
