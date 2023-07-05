b322fc61b5cf37f8d24f9216259d07148c4b22f0
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2023-07-05 08:54:44 -0300

    Adicionando biblioteca de validação Zod ao formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,28 @@
 import { Play } from "phosphor-react";
 import { CountDowContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod';
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(3, 'Informe a tarefa'),
+    minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema), 
+    });
 
     function handleCreateNewCycle(data: any) {
         
         console.log(data);
     }
+
+    console.log(formState.errors);
 
     const task = watch('task');
     const isSubmitDisabled = !task;
